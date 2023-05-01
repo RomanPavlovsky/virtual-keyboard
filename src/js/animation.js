87796a0994c7affbb2d1bff563eb250f4d480cb6
Roman.P <killme15139214@gmail.com>
2023-05-01 11:02:48 +0300

    feat: add mouse animation (Mon, May 01, 2023 11:02:48 AM)

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,8 +1,25 @@
 const keys = document.querySelectorAll(".key");
+const keyboard = document.querySelector(".keyboard");
 
 export const animation = () => {
+  let mouseActiveKey;
   document.addEventListener("keydown", (event) => {
     event.preventDefault();
+    event.stopPropagation();
+    for (const key of keys) {
+      if (event.code === key.dataset.index) {
+        key.classList.add("key_up");
+      }
+    }
+  });
+  keyboard.addEventListener("mousedown", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.target.closest(".key")) {
+      event.target.classList.add("key_up");
+      mouseActiveKey = event.target;
+    }
+
     for (const key of keys) {
       if (event.code === key.dataset.index) {
         key.classList.add("key_up");
@@ -11,11 +28,17 @@ export const animation = () => {
   });
   document.addEventListener("keyup", (event) => {
     event.preventDefault();
+    event.stopPropagation();
     for (const key of keys) {
       if (event.code === key.dataset.index) {
         key.classList.remove("key_up");
       }
     }
+  });
+  document.addEventListener("mouseup", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    mouseActiveKey.classList.remove("key_up");
   });
 };
 animation();
